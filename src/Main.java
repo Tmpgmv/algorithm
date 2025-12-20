3ebf1642f46ee75ceca83bfbec814910f87d381e
Your Name <you@example.com>
2025-12-20 21:23:48 +0300

    Init

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import programs.GeneratePresetImpl;
+
+public class Main {
+    public static void main(String[] args) {
+        var tmp = new GeneratePresetImpl();
+        tmp.generate(null, 0);
+    }
+}
